e9e1da11533ccba706d58d4f85fd7799d7a8f536
Wagner Marques Oliveira <wagnermarques00@gmail.com>
2023-02-24 10:09:18 -0300

    Toggle Theme refactored

--- webpage/main.js
+++ webpage/main.js
@@ -1,13 +1,33 @@
+import { checkTimeAndApplyTheme, toggleMode } from "./src/js/toggleTheme.js";
 import photos from "./src/constants/photos.js";
 import renderClockAndCalendar from "./src/js/clock.js";
 import renderPhoto from "./src/js/photosRender.js";
 import renderPhrase from "./src/js/phrasesRender.js";
 
+const FIVE_MINUTES = 300000;
+
 const authorTag = document.querySelector(".card__author");
 const photoTag = document.querySelector(".card__image");
 const quoteTag = document.querySelector(".card__phrase");
+const stylizableElements = {
+	backgroundToggle: document.querySelector("#dn"),
+	body: document.querySelector("body"),
+	card: document.querySelector("#card"),
+	cardImage: document.querySelector("#card__image"),
+	clock: document.querySelector("#clock-wrapper"),
+	header: document.querySelector("#header"),
+	headerTitle: document.querySelector("#header-title"),
+	player: document.querySelector("#player"),
+	playerImage: document.querySelector("#player__image"),
+};
 
-const FIVE_MINUTES = 300000;
+window.addEventListener("load", () => {
+	checkTimeAndApplyTheme(stylizableElements);
+});
+
+stylizableElements.backgroundToggle.addEventListener("click", () => {
+	toggleMode(stylizableElements);
+});
 
 renderClockAndCalendar();
 renderPhoto(photos, photoTag);
--- webpage/src/js/toggleTheme.js
+++ webpage/src/js/toggleTheme.js
@@ -1,60 +1,44 @@
-const backgroundToggle = document.getElementById("dn");
-const body = document.querySelector("body");
-const card = document.getElementById("card");
-const cardImage = document.getElementById("card__image");
-const clock = document.getElementById("clock-wrapper");
-const header = document.getElementById("header");
-const headerTitle = document.getElementById("header-title");
-const player = document.getElementById("player");
-const playerImage = document.getElementById("player__image");
-
-function setNightMode() {
-	body.classList.add("night");
-	card.classList.add("card-night");
-	cardImage.classList.add("card__image-night");
-	clock.classList.add("clock-night");
-	header.classList.add("header-night");
-	headerTitle.classList.add("header__title-night");
-	player.classList.add("player-night");
-	playerImage.classList.add("player__image-night");
-
-	backgroundToggle.checked = true;
-}
-
-function setDayMode() {
-	body.classList.remove("night");
-	card.classList.remove("card-night");
-	cardImage.classList.remove("card__image-night");
-	clock.classList.remove("clock-night");
-	header.classList.remove("header-night");
-	headerTitle.classList.remove("header__title-night");
-	player.classList.remove("player-night");
-	playerImage.classList.remove("player__image-night");
-
-	backgroundToggle.checked = false;
-}
-
-function toggleMode() {
-	if (backgroundToggle.checked) {
-		setNightMode();
-	} else {
-		setDayMode();
-	}
-}
-
-function checkTimeAndApplyTheme() {
+export function checkTimeAndApplyTheme(elements) {
 	const date = new Date();
 	const hours = date.getHours();
 
 	if (hours >= 18 || hours < 5) {
-		setNightMode();
+		setNightMode(elements);
 	} else {
-		setDayMode();
+		setDayMode(elements);
 	}
 }
 
-window.addEventListener("load", () => {
-	checkTimeAndApplyTheme();
-});
+export function toggleMode(elements) {
+	if (elements.backgroundToggle.checked) {
+		setNightMode(elements);
+	} else {
+		setDayMode(elements);
+	}
+}
 
-backgroundToggle.addEventListener("click", toggleMode);
+function setNightMode(elements) {
+	elements.body.classList.add("night");
+	elements.card.classList.add("card-night");
+	elements.cardImage.classList.add("card__image-night");
+	elements.clock.classList.add("clock-night");
+	elements.header.classList.add("header-night");
+	elements.headerTitle.classList.add("header__title-night");
+	elements.player.classList.add("player-night");
+	elements.playerImage.classList.add("player__image-night");
+
+	elements.backgroundToggle.checked = true;
+}
+
+function setDayMode(elements) {
+	elements.body.classList.remove("night");
+	elements.card.classList.remove("card-night");
+	elements.cardImage.classList.remove("card__image-night");
+	elements.clock.classList.remove("clock-night");
+	elements.header.classList.remove("header-night");
+	elements.headerTitle.classList.remove("header__title-night");
+	elements.player.classList.remove("player-night");
+	elements.playerImage.classList.remove("player__image-night");
+
+	elements.backgroundToggle.checked = false;
+}
