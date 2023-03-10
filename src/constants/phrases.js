e70ec2684eb3005de52d4a4fbe78a13783d742a7
Wagner Marques Oliveira <wagnermarques00@gmail.com>
2023-03-10 12:49:34 -0300

    Update phrases.js

--- src/constants/phrases.js
+++ src/constants/phrases.js
@@ -27,6 +27,66 @@ const phrases = [
 		quote: "O que a memória ama fica eterno. Te amo com a memória, imperecível.",
 		author: "Adélia Prado",
 	},
+	{
+		quote: "Você me ama ao cubo, eu te amo ao quadrado. Não posso dar menos do que você merece!",
+		author: "Cazuza",
+	},
+	{
+		quote: "O problema é que eu te amo. Não tenho dúvidas que com você daria certo",
+		author: "Nando Reis",
+	},
+	{
+		quote: "Te amo sem saber como, nem quando, nem onde, te amo diretamente sem problemas nem orgulho: assim te amo porque não sei amar de outra maneira.",
+		author: "Pablo Neruda",
+	},
+	{
+		quote: "Só sofro com toda a força do mundo porque amo com a intensidade de um cometa.",
+		author: "Nando Reis",
+	},
+	{
+		quote: "Mesmo estando longe, eu te amo. E amo mesmo. Mesmo não sabendo amar.",
+		author: "Caio Fernando Abreu",
+	},
+	{
+		quote: "Imagine uma nova história para sua vida e acredite nela.",
+		author: "Paulo Coelho",
+	},
+	{
+		quote: "Tudo o que um sonho precisa para ser realizado é alguém que acredite que ele possa ser realizado.",
+		author: "Roberto Shinyashiki",
+	},
+	{
+		quote: "Lute com determinação, abrace a vida com paixão, perca com classe e vença com ousadia, porque o mundo pertence a quem se atreve e a vida é muito para ser insignificante.",
+		author: "Augusto Branco",
+	},
+	{
+		quote: "Faça o que for necessário para ser feliz. Mas não se esqueça que a felicidade é um sentimento simples, você pode encontrá-la e deixá-la ir embora por não perceber sua simplicidade.",
+		author: "Martha Medeiros",
+	},
+	{
+		quote: "Tenho em mim todos os sonhos do mundo.",
+		author: "Fernando Pessoa",
+	},
+	{
+		quote: "É preciso ter o caos dentro de si para gerar uma estrela dançante.",
+		author: "Friedrich Nietzsche",
+	},
+	{
+		quote: "Pedras no caminho? Eu guardo todas. Um dia vou construir um castelo.",
+		author: "Nemo Nox",
+	},
+	{
+		quote: "A persistência é o caminho do êxito.",
+		author: "Charles Chaplin",
+	},
+	{
+		quote: "O sucesso nasce do querer, da determinação e persistência em se chegar a um objetivo. Mesmo não atingindo o alvo, quem busca e vence obstáculos, no mínimo fará coisas admiráveis.",
+		author: "José de Alencar",
+	},
+	{
+		quote: "Determinação, coragem e autoconfiança são fatores decisivos para o sucesso. Se estamos possuídos por uma inabalável determinação, conseguiremos superá-los. Independentemente das circunstâncias, devemos ser sempre humildes, recatados e despidos de orgulho.",
+		author: "Dalai Lama",
+    },
 ];
 
 export default phrases;
